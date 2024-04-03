e53d08c14a71b662db2da949380c6298d9d87f10
sridharreddyaileni <sridharreddy2508@gmail.com>
2024-04-03 22:34:01 +0530

    second commit

--- Repos-terraform/terraform-aws-vpc/main.tf
+++ Repos-terraform/terraform-aws-vpc/main.tf
@@ -73,6 +73,14 @@ resource "aws_subnet" "database" {
   )
 }
 
+resource "aws_db_subnet_group" "default" {
+  name = "${local.name}"
+  subnet_ids = aws_subnet.database[*].id
+  tags = {
+    Name = "${local.name}"
+  }
+}
+
 resource "aws_eip" "eip" {  #this is elastic ip creation
   domain = "vpc"
 }
